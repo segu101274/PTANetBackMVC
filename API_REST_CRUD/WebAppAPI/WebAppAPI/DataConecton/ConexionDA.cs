f414be36b80e00038acc8c05d375fa2c9a64dd29
segu101274 <svigo@hotmail.com>
2024-07-15 01:49:34 +0200

    SE AGREGO EL DOCKER

--- API_REST_CRUD/WebAppAPI/WebAppAPI/DataConecton/ConexionDA.cs
+++ API_REST_CRUD/WebAppAPI/WebAppAPI/DataConecton/ConexionDA.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Data.SqlClient;
 namespace WebAppAPI.DataConecton
 {
+    
     public class ConexionDA
     {
-        SqlConnection cn = new SqlConnection(@"Data Source=localhost, 1433;Initial Catalog=Test;User=sa;Password=*123Test*;" +
-           "MultipleActiveResultSets = True;TrustServerCertificate= False;Encrypt= False");
+
+        SqlConnection cn = new SqlConnection(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"));
+
         public SqlConnection getcn
         {
             get { return cn; }
--- API_REST_CRUD/WebAppAPI/WebAppAPI/Program.cs
+++ API_REST_CRUD/WebAppAPI/WebAppAPI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(
